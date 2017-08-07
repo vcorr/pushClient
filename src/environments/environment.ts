4c8287a6e93e4c17f6bd1f94429ba0b5ef588b34
Vasco Correia <vasco.correia@netum.fi>
2017-08-07 16:03:55 +0300

    added firebase config and initialization

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,6 +3,15 @@
 // `ng build --env=prod` then `environment.prod.ts` will be used instead.
 // The list of which env maps to which file can be found in `.angular-cli.json`.
 
+
 export const environment = {
-  production: false
+  production: false,
+  firebase: {
+    apiKey: "AIzaSyAdzI2zDpvWaOpAHjt1EHdqm1Ao7tj3IPs",
+    authDomain: "pushproject-a9afe.firebaseapp.com",
+    databaseURL: "https://pushproject-a9afe.firebaseio.com",
+    projectId: "pushproject-a9afe",
+    storageBucket: "pushproject-a9afe.appspot.com",
+    messagingSenderId: "785149027944"
+  }
 };
