6dce342723a6339e7894cf531fe47da13bfb8f18
Vasco Correia <vasco.correia@netum.fi>
2017-08-07 16:15:50 +0300

    added firebase props to prod config

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,12 @@
 export const environment = {
-  production: true
+  production: true,
+  firebase: {
+    apiKey: "AIzaSyAdzI2zDpvWaOpAHjt1EHdqm1Ao7tj3IPs",
+    authDomain: "pushproject-a9afe.firebaseapp.com",
+    databaseURL: "https://pushproject-a9afe.firebaseio.com",
+    projectId: "pushproject-a9afe",
+    storageBucket: "pushproject-a9afe.appspot.com",
+    messagingSenderId: "785149027944"
+  }
+
 };
