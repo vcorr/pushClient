aae05f4ede7b556cf3af08f576b18a91452054bf
Vasco Correia <vasco.correia@netum.fi>
2017-08-08 15:52:46 +0300

    added logic show subscription status and way to subscribe for push notifications

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,8 @@
 import {Component} from '@angular/core';
+import {FirebaseApp} from 'angularfire2';
+import 'firebase/messaging';
+
+declare var Notification: any;
 
 @Component({
   selector: 'app-root',
@@ -7,11 +11,15 @@ import {Component} from '@angular/core';
 })
 export class AppComponent {
   title = 'app';
-  notRegistered = true;
   private swRegistration: ServiceWorkerRegistration;
+  private firebaseApp;
+  private firebaseMessaging;
 
+  public notificationStatus = Notification.permission;
 
-  constructor() {
+  constructor(firebaseApp:FirebaseApp) {
+    this.firebaseApp = firebaseApp;
+
     if (navigator.serviceWorker && "PushManager" in window) {
       navigator.serviceWorker.register('/sw.js')
         .then((registration) => {
@@ -21,7 +29,58 @@ export class AppComponent {
         .catch(function (err) {
           console.error('Unable to register service worker.', err);
         });
-
     }
+
   }
+
+  public registerForPush() {
+      this.initFireBase();
+  }
+
+
+  private initFireBase() {
+
+    console.log("asking for permission");
+
+    this.firebaseMessaging = this.firebaseApp.messaging();
+
+    console.log(this.firebaseMessaging);
+    this.firebaseMessaging.useServiceWorker(this.swRegistration);
+    this.firebaseMessaging.requestPermission()
+      .then(() => {
+        console.log('Notification permission granted.');
+        this.getToken();
+        this.notificationStatus = 'granted';
+      })
+      .catch((err) => {
+        console.error(err);
+      });
+
+    this.firebaseMessaging.onTokenRefresh(()=>{
+      console.log("token REFRESH");
+    })
+
+    this.firebaseMessaging.onMessage((message)=>{
+      console.log("MESSAGE ARRIVED");
+      console.log(message);
+    })
+  }
+
+  private getToken(): void {
+    console.log("Get token from user");
+
+    this.firebaseMessaging.getToken()
+      .then((currentToken) => {
+        console.log("got token from user's device");
+        console.log(currentToken);
+        //TODO: send token to server
+      })
+      .catch((err) => {
+        console.error("token failed");
+        console.error(err);
+
+      });
+  }
+
+
 }
